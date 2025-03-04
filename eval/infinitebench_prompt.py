e5319fa353a4f590ab5a8651fa783cb380bf7ca1
“ssuminan” <wsdvbnm13@gmail.com>
2025-03-04 22:15:08 +0900

    initial commit

--- eval/infinitebench_prompt.py
+++
@@ -1,4 +0,0 @@
-yarn_mistral_templates = {
-    "longbook_choice_eng": "Read the book and answer the question.\n\n{context}\n\nQuestion: {question}\nA. {OPTION_A}\nB. {OPTION_B}\nC. {OPTION_C}\nD. {OPTION_D}\n\nThe letter of the correct answer is ",
-    "longbook_qa_eng": "Read the book and answer the question. Be very concise in your answer.\n\n{context}\n\nQuestion: {question}\nAnswer: ",
-}
